5a8575b1809c7159ce347596efe14280ea7569d5
jason.zou@gmail.com <jason.zou@gmail.com>
2013-08-07 23:15:58 -0400

    update stop script

--- stop.sh
+++ stop.sh
@@ -7,4 +7,11 @@ do
    kill -9 $PID
    sleep 1
 done
+PIDs=`ps -ef|grep ingest.py|grep -v grep|awk '{print $2}'`
+for PID in $PIDs
+do
+   echo "Kill a process: $PID"
+   kill -9 $PID
+   sleep 1
+done
 echo "Done."
